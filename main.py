760b626c4315f74ebc05885c17362d4332b033a2
Nick McLarnan <nickgeek@gmail.com>
2014-05-24 18:26:50 -0400

    split out terrain gen into new file

--- main.py
+++ main.py
@@ -22,29 +22,13 @@ from numpy import random
 from command import *
 from camera  import Camera
 from loaders import *
+from terrain import genTerrain
 
 HEIGHT = 800
 WIDTH = 1280
 
 null = c_void_p(0) #handy trick from https://bitbucket.org/tartley/gltutpy/
 
-def genTerrain( length, breadth):
-   verts = []
-   indices = []
-   _current_index = 0
-
-   for x in range(length):
-      for z in range(breadth):
-         verts.append( [x,   0.0, z, ] )
-         if x >= 1 and z >= 1:
-            indices.append( [ _current_index, _current_index - 1, _current_index - breadth ])
-            indices.append( [ _current_index - breadth - 1, _current_index - 1, _current_index - breadth ])
-         _current_index += 1
-
-
-   vert_vbo = vbo.VBO( array(verts, dtype='f') )
-   index_vbo = vbo.VBO( array(indices, dtype=numpy.int32), target = GL_ELEMENT_ARRAY_BUFFER )
-   return vert_vbo, index_vbo
 
 pygame.init()
 screen = pygame.display.set_mode((WIDTH,HEIGHT), HWSURFACE|OPENGL|DOUBLEBUF|OPENGLBLIT)
